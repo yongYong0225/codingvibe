a05679e61d77d233395fb90ad380f57864f22a55
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-20 03:04:02 +0900

    h2콘솔 연결 해제

--- src/main/java/com/sparta/myboard/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/myboard/config/WebSecurityConfig.java
@@ -32,13 +32,13 @@ public class WebSecurityConfig {
     }
 
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // h2-console 사용 및 resources 접근 허용 설정
-        return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        // h2-console 사용 및 resources 접근 허용 설정
+//        return (web) -> web.ignoring()
+//                .requestMatchers(PathRequest.toH2Console())
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
