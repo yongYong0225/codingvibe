c5e73d2bcc028aafa221f64c95047c21ae44a17e
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-20 02:09:48 +0900

    COR 적용(demo)

--- src/main/java/com/sparta/myboard/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/myboard/config/WebSecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
@@ -57,7 +60,72 @@ public class WebSecurityConfig {
         http.formLogin().loginPage("/api/user/login-page").permitAll();
 
         http.exceptionHandling().accessDeniedPage("/api/user/forbidden");
+
+        http
+//                .exceptionHandling()
+//                .authenticationEntryPoint(customAuthenticationEntryPoint())
+//                .accessDeniedHandler(customAccessDeniedHandler())
+//                .and()
+                .headers()
+                .frameOptions()
+                .disable()
+
+                // 이 설정을 해주지 않으면 밑의 corsConfigurationSource가 적용되지 않습니다!
+                .and()
+                .cors()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//                .and()
+//                .apply(new JwtConfig(jwtUtil, om));
+
         return http.build();
+    }
+
+    /**
+     * 이 설정을 해주면, 우리가 설정한대로 CorsFilter가 Security의 filter에 추가되어
+     * 예비 요청에 대한 처리를 해주게 됩니다.
+     * cors 개념 참고 - https://inpa.tistory.com/entry/WEB-%F0%9F%93%9A-CORS-%F0%9F%92%AF-%EC%A0%95%EB%A6%AC-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95-%F0%9F%91%8F
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 서버에서 응답하는 리소스에 접근 가능한 출처를 명시
+        // Access-Control-Allow-Origin
+        //config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://charleybucket.s3-website.ap-northeast-2.amazonaws.com"); //요거 변경하시면 됩니다.
+
+        // 특정 헤더를 클라이언트 측에서 꺼내어 사용할 수 있게 지정
+        // 만약 지정하지 않는다면, Authorization 헤더 내의 토큰 값을 사용할 수 없음
+        // Access-Control-Expose-Headers
+        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
+
+        // 본 요청에 허용할 HTTP method(예비 요청에 대한 응답 헤더에 추가됨)
+        // Access-Control-Allow-Methods
+        config.addAllowedMethod("*");
+
+        // 본 요청에 허용할 HTTP header(예비 요청에 대한 응답 헤더에 추가됨)
+        // Access-Control-Allow-Headers
+        config.addAllowedHeader("*");
+
+        // 기본적으로 브라우저에서 인증 관련 정보들을 요청 헤더에 담지 않음
+        // 이 설정을 통해서 브라우저에서 인증 관련 정보들을 요청에 담을 수 있도록 해줍니다.
+        // Access-Control-Allow-Credentials
+        config.setAllowCredentials(true);
+
+        // allowCredentials 를 true로 하였을 때,
+        // allowedOrigin의 값이 * (즉, 모두 허용)이 설정될 수 없도록 검증합니다.
+        config.validateAllowCredentials();
+
+        // 어떤 경로에 이 설정을 적용할 지 명시합니다. (여기서는 전체 경로)
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
 }
